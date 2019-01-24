ef42d62ad9f93e5e1be1517a5dd4cf28523f97f8
Bhupinder Singh <33602866+chepecali@users.noreply.github.com>
2019-01-24 23:03:44 +0530

    Update app.js

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import firebase from 'firebase';
+import firebase from '@firebase/app';
+import '@firebase/auth';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  componentWillMount() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyBtxMY4K6uHxv_2e3GD-FWAD2ACX6lPVRE',
-      authDomain: 'authentication-70a18.firebaseapp.com',
-      databaseURL: 'https://authentication-70a18.firebaseio.com',
-      storageBucket: 'authentication-70a18.appspot.com',
-      messagingSenderId: '682333809338'
-    });
-  }
+	componentWillMount() {
+	firebase.initializeApp({
+    apiKey: 'AIzaSyBmhOdu7gjS7pD6IFQLcqPQKYbJX5iLrl0',
+    authDomain: 'auth-89bb4.firebaseapp.com',
+    databaseURL: 'https://auth-89bb4.firebaseio.com',
+    projectId: 'auth-89bb4',
+    storageBucket: 'auth-89bb4.appspot.com',
+    messagingSenderId: '509190226769'
+  });
+}
 
-  render() {
-    return (
-      <View>
-        <Header headerText="Authentication" />
-        <LoginForm />
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View>
+			<Header headerText="Authentication" />
+				<LoginForm />
+			</View>
+		);
+	}
 }
 
 export default App;
